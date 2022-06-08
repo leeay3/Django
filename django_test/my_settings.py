c347553ce90be6d4907f81ab333f14ad1a838fc0
leeay3 <dkdud5135@naver.com>
2022-06-08 16:53:04 +0900

    Add : Django Project Setting

---
+++ django_test/my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'dkfkfl1643!!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+				'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
+
+SECRET_KEY = 'django-insecure-vik9e(z0upd0@zv265qma&f@_2e_xk9t*nqx89h+)o91^bdyr_'
