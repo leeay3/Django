cc69feb9c53435dbf0e6cd97791083e8050c8dfd
leeay3 <dkdud5135@naver.com>
2022-06-16 00:50:35 +0900

    Add : model, Modify : view

--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'corsheaders',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
