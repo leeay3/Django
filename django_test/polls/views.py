8a10e798c16d9c85bf13f154f4f3389b3097c354
leeay3 <dkdud5135@naver.com>
2022-06-17 23:56:21 +0900

    Modify : 404 error

--- django_test/polls/views.py
+++ django_test/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -34,3 +35,10 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
